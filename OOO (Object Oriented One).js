69be394cb72dfbd99bddeeb2af515118b282a4bb
Matheus Cesar <matheuscesar2000@gmail.com>
2020-11-23 00:58:28 -0300

    One

---
+++ OOO (Object Oriented One).js
@@ -0,0 +1,40 @@
+/**
+ * @description One
+ * 
+ * _'->' means returns_
+ * @example new One() -> {one: 1}
+ * getOne() -> 1
+ * setOne(<Number>) -> set value -> 1
+ * 
+ * @returns {Object} {one: 1}
+ */
+class One{
+    constructor(){
+        this.one = 1;
+        return this.one
+    }
+
+    /**
+     * @description set one to desired value. Value must be '1'
+     * @param {Number} newOne one
+     * @example setOne(1) -> 1
+     */
+    setOne(newOne){
+        if(!typeof newOne == "number" || newOne != 1){
+            return new Error("Value must be 1")
+        }
+        else{
+            this.one = newOne;
+            return this.one;
+        }
+    }
+
+    /**
+     * @description get actual one
+     */
+    getOne(){
+        return this.one
+    }
+}
+
+i = new One().getOne() + i
